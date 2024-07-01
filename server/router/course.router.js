94be722ac7bcc46c2a5495777609f61b5a1ae521
Rawana Khalid Mahmoud <112649734+rawanakhalid64@users.noreply.github.com>
2024-07-01 16:32:45 +0300

    added star

--- server/router/course.router.js
+++ server/router/course.router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// Import the getAllCourses controller
+// Import the getAllCourses controller*
 const { getAllCourses } = require('../controllers/courses');
 
 // Define the route for getting all courses
